4639bda072f93de75eb88125ce32c670a976661e
albaintor <118518828+albaintor@users.noreply.github.com>
2026-03-04 12:39:25 +0100

    Added browsing method

--- ucapi/entity.py
+++ ucapi/entity.py
@@ -147,3 +147,22 @@ class Entity:
             return await handler(self, cmd_id, params, websocket=websocket)
 
         return await handler(self, cmd_id, params)
+
+    # pylint: disable=W0613
+    async def browse_media(
+        self,
+        params: dict[str, Any],
+        *,
+        websocket: Any,
+    ) -> dict[str, Any] | StatusCodes:
+        """
+        Execute entity browsing request.
+
+        Returns NOT_IMPLEMENTED if no handler is installed.
+
+        :param params: browsing parameters
+        :param websocket: optional websocket connection. Allows for directed event
+                          callbacks instead of broadcasts.
+        :return: browsing response or status code if any error occurs
+        """
+        return StatusCodes.NOT_IMPLEMENTED
